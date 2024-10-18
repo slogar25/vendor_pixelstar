71d6a75a9927a650a667858a7ed5375455d8a48d
Michael Bestas <mkbestas@lineageos.org>
2024-10-18 19:57:45 +0300

    config: Changes for libfmjni bp conversion
    Change-Id: I666bdc4c1d0cdab4d2557c07f32466cceabe8439

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -69,6 +69,7 @@ SOONG_CONFIG_pixelstarNvidiaVars += \
 
 SOONG_CONFIG_NAMESPACES += pixelstarQcomVars
 SOONG_CONFIG_pixelstarQcomVars += \
+    no_fm_firmware \
     qti_vibrator_effect_lib \
     qti_vibrator_use_effect_stream \
     supports_extended_compress_format \
@@ -94,6 +95,7 @@ SOONG_CONFIG_pixelstarNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEW
 SOONG_CONFIG_pixelstarQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_pixelstarQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_pixelstarQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_pixelstarQcomVars_no_fm_firmware := $(TARGET_QCOM_NO_FM_FIRMWARE)
 SOONG_CONFIG_pixelstarGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
 SOONG_CONFIG_pixelstarGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_pixelstarGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
@@ -151,3 +153,18 @@ else
 SOONG_CONFIG_pixelstarQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
 endif
 SOONG_CONFIG_pixelstarQcomVars_qti_vibrator_effect_lib := $(TARGET_QTI_VIBRATOR_EFFECT_LIB)
+
+# libfmjni
+ifeq ($(BOARD_HAVE_QCOM_FM),true)
+    PRODUCT_SOONG_NAMESPACES += \
+        vendor/qcom/opensource/libfmjni
+else ifeq ($(BOARD_HAVE_BCM_FM),true)
+    PRODUCT_SOONG_NAMESPACES += \
+        hardware/broadcom/fm
+else ifeq ($(BOARD_HAVE_SLSI_FM),true)
+    PRODUCT_SOONG_NAMESPACES += \
+        hardware/samsung_slsi/fm
+else ifneq ($(BOARD_HAVE_MTK_FM),true)
+    PRODUCT_SOONG_NAMESPACES += \
+        packages/apps/FMRadio/jni/fmr
+endif
