02bf214423b52998daae8321d5606695b77dc6e3
slogar25 <michaelslogar@gmail.com>
2024-12-12 11:07:06 +0000

    pixelstar: remove kernel build task
    We will move fully to an AOSP build system with prebuilt kernel
    
    Change-Id: I19a0ab0be1154780aa8b0bf46cdc1272a1efd7e1
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/8176
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- config/BoardConfigPixelstar.mk
+++ config/BoardConfigPixelstar.mk
@@ -1,8 +1,6 @@
 # Recovery
 BOARD_USES_FULL_RECOVERY_IMAGE ?= true
 
-include vendor/pixelstar/config/BoardConfigKernel.mk
-
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
